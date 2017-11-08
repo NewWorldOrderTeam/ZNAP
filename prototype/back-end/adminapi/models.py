22688cad391c2864bbd54d41088a96d4220bb3b6
Nazar <nazarko1805@gmail.com>
2017-11-08 19:14:02 +0200

    [^] Queue some work

--- prototype/back-end/adminapi/models.py
+++ prototype/back-end/adminapi/models.py
@@ -3,9 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from model_utils import Choices
+
 
 class Admin(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=50)
     def __str__(self):
         return str(self.email)
+
+class Queue(models.Model):
+    nameOFCNAP = Choices(
+        ('1', 'first')
+    )
+    adressOFCNAP = Choices(
+        ('1', 'NULP')
+    )
+    nameOfDepartment = models.CharField(max_length=1, choices=nameOFCNAP)
+    location = models.CharField(max_length=1, choices=adressOFCNAP)
--- prototype/back-end/adminapi/views.py
+++ prototype/back-end/adminapi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
