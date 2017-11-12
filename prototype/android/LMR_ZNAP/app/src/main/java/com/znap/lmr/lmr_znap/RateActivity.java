911bca5a9381f17102f23e2bcbf16036b5b43981
andyblyzniuk <andrianblyzniuk@gmail.com>
2017-11-12 18:14:34 +0200

    RateActvity, Rate UI Android

---
+++ prototype/android/LMR_ZNAP/app/src/main/java/com/znap/lmr/lmr_znap/RateActivity.java
@@ -0,0 +1,18 @@
+package com.znap.lmr.lmr_znap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andy Blyzniuk on 12.11.2017.
+ */
+
+public class RateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+
+    }
+}
