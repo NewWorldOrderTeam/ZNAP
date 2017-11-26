ad188eeb2d2085549a03660a97c3351278b06349
andyblyzniuk <andrianblyzniuk@gmail.com>
2017-11-26 17:36:01 +0200

    Android Manfest fixes

--- prototype/android/LMR_ZNAP/app/src/main/java/com/znap/lmr/lmr_znap/Item.java
+++ prototype/android/LMR_ZNAP/app/src/main/java/com/znap/lmr/lmr_znap/Item.java
@@ -9,12 +9,16 @@ public class Item {
     public final String first_name;
     public final String last_name;
     public final String middle_name;
+    public final String telephone_number;
+    public final String email;
 
-    public Item(int id, String first_name, String last_name, String middle_name) {
+    public Item(int id, String first_name, String last_name, String middle_name, String telephone_number, String email) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.middle_name = middle_name;
+        this.telephone_number = telephone_number;
+        this.email = email;
 
     }
 
--- prototype/android/LMR_ZNAP/app/src/main/java/com/znap/lmr/lmr_znap/Request.java
+++ prototype/android/LMR_ZNAP/app/src/main/java/com/znap/lmr/lmr_znap/Request.java
@@ -1,9 +1,5 @@
 package com.znap.lmr.lmr_znap;
 
-import android.content.ClipData;
-
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -11,7 +7,6 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Created by Andy Blyzniuk on 01.11.2017.
@@ -27,6 +22,7 @@ public interface Request {
     @FormUrlEncoded
     @POST("/api/v1.0/addrate/")
     Call<User> addrate(@Field("description") String leaveReview);
-    @GET("/api/v1.0/register")
-    Response getInfo(@Query("mid") String first_name, String last_name, String middle_name, Callback<Response> callback);
+    @FormUrlEncoded
+    @GET("/api/v1.0/user/")
+    Response getInfo(@Field("first_name") String first_name, @Field("last_name") String last_name, @Field("middle_name") String middle_name, @Field("phone") String telephone_number, @Field("email") String email, Callback<Response> callback);
 }
