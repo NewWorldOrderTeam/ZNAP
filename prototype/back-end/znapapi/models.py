4d43bfe172e7056bd0a3580356088d04035b8174
iotvl <vladik2395@gmail.com>
2017-12-01 23:45:04 +0200

    [^][Back-end] change str() to unciode()

--- prototype/back-end/znapapi/models.py
+++ prototype/back-end/znapapi/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Znap(models.Model):
     name = models.CharField(max_length=100)
-    def __str__(self):
-        return str(self.id)
+    def __unicode__(self):
+        return self.name
+
 
