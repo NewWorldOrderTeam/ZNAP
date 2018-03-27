d09f4c453c685277652f26686523097234194872
iotvl <vladik2395@gmail.com>
2018-03-27 19:13:15 +0300

    [+][Back-end] Convert registration check from html to pdf

--- prototype/back-end/znap/urls.py
+++ prototype/back-end/znap/urls.py
@@ -91,5 +91,5 @@ urlpatterns = [
     url(r'^api/v1.0/cnap/(?P<service_center>[0-9]+)/group/', QlogicGroupViewSet.as_view(), name='cnap groups'),
     url(r'^api/v1.0/cnap/register/', QlogicFinishRegistration.as_view(), name='finish registration to cnap'),
     url(r'^api/v1.0/cnap/', QlogicCnapViewSet.as_view(), name='cnap'),
-    url(r'^queue/', QlogicQueueStateViewSet.as_view(), name='loh')
+    url(r'^api/v1.0/queue/', QlogicQueueStateViewSet.as_view(), name='loh')
 ]
